383245b22d0fddc6b938d24ca7c9345a4b7348bd
SuezDevelopment <olayode.gideon@calebuniversity.edu.ng>
2023-11-19 19:44:51 +0100

    pushh

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,14 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://my-cardlysupply.com</homePageLink>
+		<homePageLink>https://</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://my-cardlysupply.com/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://my-cardlysupply.com/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://my-cardlysupply.com/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://my-cardlysupply.com/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://my-cardlysupply.com/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https:///xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https:///xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https:///wp-json/" />
 			</apis>
 	</service>
 </rsd>
-	
